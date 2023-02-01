93ac0c2fd05c06420a82bd935b5ef6d1c3192f75
Bagas Ramadhani <bagasrmdhn14@gmail.com>
2023-02-01 15:04:15 +0700

    initial commit

---
+++ controllers/UserController.js
@@ -0,0 +1,57 @@
+import User from "../models/UserModel.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export const getUsersById = async (req, res) => {
+  try {
+    const response = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    await User.create(req.body);
+    res.status(201).json({ msg: "user created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "user updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "user deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ models/UserModel.js
@@ -0,0 +1,36 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const DataTypes = Sequelize;
+
+const User = db.define(
+  "users",
+  {
+    name: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default User;
+
+(async () => {
+  await db.sync();
+})();
